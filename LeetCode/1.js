d2cfd97c76cac177957ad789b9320241c6161f6e
John Chon <johnchon94@gmail.com>
2022-08-18 09:35:57 -0700

    two sums

---
+++ LeetCode/1.js
@@ -0,0 +1,13 @@
+// TWO SUM
+
+function twoSum(nums, target) {
+  const map = {};
+  
+  for (let i = 0; i < nums.length; i++) {
+      if (map[target - nums[i]] !== undefined) {
+          return [map[target - nums[i]], i];
+      } else {
+          map[nums[i]] = i;
+      }
+  }
+}
